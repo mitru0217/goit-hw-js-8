72c764df2d4e347a60b2d700d3eaf9541fef39bb
Michael <mitru0217@gmail.com>
2022-08-19 19:48:26 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup (galleryItems);
+
+imageContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+imageContainer.addEventListener("click", onImageContainerClick)
+
+function createGalleryCardsMarkup (galleryItems) {
+    return galleryItems
+    .map (({preview, original,description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+    })
+    .join("");
+}
+
+function onImageContainerClick (e) {
+  e.target.alt;
+}
