a1b1692dd6d664dd68db6695fe14b0046b7af8dd
Michael <mitru0217@gmail.com>
2022-08-21 21:28:16 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,19 +6,22 @@ let formData = savedInfo ?? {};
 
 const form = document.querySelector(".feedback-form");
 
+// console.log(form.elements.name);
+
 form.addEventListener("input", throttle(onFormChange, 500));
 
 form.addEventListener("submit", onFormSubmit);
  
  function onFormChange (e) {
     formData[e.target.name] = e.target.value;
-
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+
  };  
 
 function onFormSubmit(e) {
     e.preventDefault();
-    if(e.target.name === "") {
+    const keys = Object.keys(formData);
+    if(keys === "") {
         return alert("Please fill all fields");
     }
     console.log (formData);
