83411ec7907ce4b5fd8ca1f62626bf3ab28f8e38
Michael <mitru0217@gmail.com>
2022-08-22 22:37:36 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,15 +3,14 @@ const STORAGE_KEY = `feedback-form-state`;
 const savedInfo = JSON.parse(localStorage.getItem(STORAGE_KEY));
 let formData = savedInfo ?? {};
 
-
 const form = document.querySelector(".feedback-form");
-
-// console.log(form.elements.name);
 
 form.addEventListener("input", throttle(onFormChange, 500));
 
 form.addEventListener("submit", onFormSubmit);
- 
+
+saveLocaleStorage();
+
  function onFormChange (e) {
     formData[e.target.name] = e.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
@@ -20,11 +19,17 @@ form.addEventListener("submit", onFormSubmit);
 
 function onFormSubmit(e) {
     e.preventDefault();
-      if (form.name === "") {
+      if (e.currentTarget.email.value === "" || e.currentTarget.message.value === "") {
         return alert("Please fill in all the fields!");
-      }
-      console.log(form.name.value);
-    e.target.reset();
+      };
+    
+    e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
     formData = {};  
 };
+function saveLocaleStorage() {
+if(savedInfo) {
+  form.elements.email.value = savedInfo.email;
+  form.elements.message.value = savedInfo.message;
+}
+}
